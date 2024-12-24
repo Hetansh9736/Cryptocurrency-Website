ce70de6d86e97f4448c4c96ea3b7221d580973e4
Hetansh <hetanshsachaniya07@gmail.com>
2024-12-24 15:17:13 +0530

    Footer and Image added

---
+++ script.js
@@ -0,0 +1,13 @@
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        'x-chain': 'solana',
+        'X-API-KEY': 'cd811768f7434753b1d85878d8c34196'
+    }
+};
+
+fetch('https://public-api.birdeye.so/defi/tokenlist?sort_by=v24hUSD&sort_type=desc&offset=0&limit=50&min_liquidity=100', options)
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
