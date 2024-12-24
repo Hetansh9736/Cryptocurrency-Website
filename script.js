2ba1e8a2563fe1dedf55aa12573e05709169dc3b
bhumit <bhumitsachaniya1010@gmail.com>
2024-12-24 15:42:17 +0530

    asynk converted

--- script.js
+++ script.js
@@ -7,7 +7,23 @@ const options = {
     }
 };
 
-fetch('https://public-api.birdeye.so/defi/tokenlist?sort_by=v24hUSD&sort_type=desc&offset=0&limit=50&min_liquidity=100', options)
-    .then(res => res.json())
-    .then(res => console.log(res))
-    .catch(err => console.error(err));
+// fetch('https://public-api.birdeye.so/defi/tokenlist?sort_by=v24hUSD&sort_type=desc&offset=0&limit=50&min_liquidity=100', options)
+//     .then(res => res.json())
+//     .then(res => console.log(res))
+//     .catch(err => console.error(err));
+let = 0;
+
+document.addEventListener('DOMContentLoaded', async ()=>{
+
+    const response = await fetch('https://public-api.birdeye.so/defi/tokenlist?sort_by=v24hUSD&sort_type=desc&offset=0&limit=50&min_liquidity=100', options);
+    const data = await response.json();
+  
+    
+    console.log(data);
+    console.log(data.tokens);
+
+
+
+
+})
+
